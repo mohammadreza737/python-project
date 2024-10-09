10fcbb7afce77b93f40eeccc445508ae69a3ce71
Mohammadreza <sadeghimohammadreza737@gmail.com>
2024-10-09 16:20:38 +0330

    Add files via upload
    بازی حدس کامپیوتر

---
+++ game2.py
@@ -0,0 +1,16 @@
+import random
+a=1
+c=99
+hads = int(random.randint(a,c))
+print(hads)
+javab = str(input('give me yor idea: '))
+while javab != ('d'):
+    if javab == ('b'):
+        a == hads
+    elif javab == ('k'):
+        c == hads
+    hads = random.randint(a,c)
+    print(hads)
+    javab = str(input('give me yor idea: '))
+print('barandeh')
+
