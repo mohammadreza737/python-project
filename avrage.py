ed80bc8007d0c66338497436447c4536fa412223
Mohammadreza <sadeghimohammadreza737@gmail.com>
2024-10-09 16:22:17 +0330

    Add files via upload

---
+++ avrage.py
@@ -0,0 +1,8 @@
+jam = counter = 0
+adad = int(input('adad ra vared koned: '))
+while adad != -1:
+    jam = adad + jam
+    counter = counter + 1
+    adad = int(input('adad ra vared koned: '))
+print(jam/counter)
+
